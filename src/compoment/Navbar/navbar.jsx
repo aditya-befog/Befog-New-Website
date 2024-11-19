c1df53597e850791edfc6d5a2c3fa512d11a0eb3
vinay2003 <vinaysharma31681@gmail.com>
2024-11-19 08:02:39 +0530

    completed the all pages

--- src/compoment/Navbar/navbar.jsx
+++ src/compoment/Navbar/navbar.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/befog_logo.svg";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes, FaGlobe, FaChevronDown } from "react-icons/fa";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedCountry, setSelectedCountry] = useState("Select Country");
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleCountrySelect = (country) => {
+    setSelectedCountry(country);
+    setIsDropdownOpen(false);
   };
 
   return (
@@ -16,13 +27,62 @@ const Navbar = () => {
         <img src={logo} alt="Befog Logo" className="logo" />
       </div>
 
-      <ul className={isOpen ? "nav-links open" : "nav-links"} onClick={() => isOpen && toggleMenu()}>
-        <li><a href="/">Home</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/about">About Us</a></li>
-        <li><a href="/contact">Contact Us</a></li>
+      <ul
+        className={isOpen ? "nav-links open" : "nav-links"}
+        onClick={() => isOpen && toggleMenu()}
+      >
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/contact">Contact Us</a>
+        </li>
+        <li
+          className="dropdown"
+          onMouseEnter={toggleDropdown}
+          onMouseLeave={toggleDropdown}
+        >
+          <button className="dropdown-toggle">
+            <FaGlobe className="world-icon" />
+            <span>{selectedCountry}</span>
+            <FaChevronDown className="dropdown-icon" />
+          </button>
+          {isDropdownOpen && (
+            <ul className="dropdown-menu">
+              <li
+                onClick={() => handleCountrySelect("India")}
+                className="dropdown-item"
+              >
+                India
+              </li>
+              <li
+                onClick={() => handleCountrySelect("USA")}
+                className="dropdown-item"
+              >
+                USA
+              </li>
+              <li
+                onClick={() => handleCountrySelect("Canada")}
+                className="dropdown-item"
+              >
+                Canada
+              </li>
+              <li
+                onClick={() => handleCountrySelect("Germany")}
+                className="dropdown-item"
+              >
+                Germany
+              </li>
+            </ul>
+          )}
+        </li>
       </ul>
-
       <button
         className="menu-icon"
         onClick={toggleMenu}
